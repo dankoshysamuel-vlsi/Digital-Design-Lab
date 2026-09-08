366f195db47e416265f901b8b8e4f9329289a7eb
Dan Samuel <dansamuelx@gmail.com>
2026-09-08 20:45:53 +0530

    Add Half Adder Testbench

---
+++ 01_Half_Adder/half_adder_tb.v
@@ -0,0 +1,27 @@
+module half_adder_tb;
+
+reg A;
+reg B;
+
+wire Sum;
+wire Carry;
+
+half_adder uut (
+    .A(A),
+    .B(B),
+    .Sum(Sum),
+    .Carry(Carry)
+);
+
+initial begin
+    $display("A B | Sum Carry");
+
+    A = 0; B = 0; #10;
+    A = 0; B = 1; #10;
+    A = 1; B = 0; #10;
+    A = 1; B = 1; #10;
+
+    $finish;
+end
+
+endmodule
